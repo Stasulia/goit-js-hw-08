1e3fda4ab8bff641080439282aa0906ad10b28b8
Stasulia <134652473+Stasulia@users.noreply.github.com>
2023-10-09 20:43:53 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+//import 
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LS_KEY = "videoplayer-current-time";
+
+const getCurrentTime = function(data) {
+   const currentTime = data.seconds;
+   localStorage.setItem(LS_KEY, JSON.stringify(currentTime));
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(LS_KEY)) || 0);
